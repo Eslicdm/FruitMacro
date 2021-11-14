8c45bf2161d05fda4e8f95e1523675e893579eff
Eslicdm <eslicdm1@gmail.com>
2021-11-14 18:44:37 -0300

    Search Bar List

--- app/src/main/java/com/eslirodrigues/fruitmacro/screen/FruitScreen.kt
+++ app/src/main/java/com/eslirodrigues/fruitmacro/screen/FruitScreen.kt
@@ -2,17 +2,31 @@ package com.eslirodrigues.fruitmacro.screen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.eslirodrigues.fruitmacro.R
 import com.eslirodrigues.fruitmacro.ui.theme.LightMagenta
 import com.eslirodrigues.fruitmacro.util.FruitApiState
 import com.eslirodrigues.fruitmacro.viewmodel.FruitViewModel
@@ -22,36 +36,71 @@ import com.eslirodrigues.fruitmacro.viewmodel.FruitViewModel
 fun FruitScreen(
     viewModel: FruitViewModel = hiltViewModel()
 ) {
-    Column(
-        modifier = Modifier
-            .background(LightMagenta)
-            .fillMaxSize()) {
-        when(val result = viewModel.response.value) {
-            is FruitApiState.Success -> {
-                LazyVerticalGrid(cells = GridCells.Fixed(2)) {
-                    items(result.data) { response ->
-                        FruitListItem(fruit = response)
+    var inputSearch by remember { mutableStateOf("") }
+
+    Scaffold() {
+        Column(
+            modifier = Modifier
+                .background(LightMagenta)
+                .fillMaxSize()
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(.9f)
+                        .padding(8.dp),
+                    value = inputSearch,
+                    onValueChange = {
+                        inputSearch = it
+                        viewModel.searchFruitList(it)
+                    },
+                    label = {
+                        Text(text = stringResource(id = R.string.search))
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = stringResource(id = R.string.search)
+                        )
+                    },
+                    maxLines = 1,
+                    singleLine = true
+                )
+                Icon(
+                    modifier = Modifier
+                        .size(44.dp)
+                        .padding(top = 16.dp)
+                        .clickable {
+                                   /* TODO */
+                        },
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = stringResource(id = R.string.more)
+                )
+            }
+            when(val result = viewModel.response.value) {
+                is FruitApiState.Success -> {
+                    LazyVerticalGrid(cells = GridCells.Fixed(2)) {
+                        items(result.data) { response ->
+                            FruitListItem(fruit = response)
+                        }
                     }
                 }
-            }
-            is FruitApiState.Failure -> {
-                Text(text = "${result.msg}")
-            }
-            is FruitApiState.Loading -> {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                is FruitApiState.Failure -> {
+                    Text(text = "${result.msg}")
                 }
-            }
-            is FruitApiState.Empty -> {
-                Text(text = "Research")
+                is FruitApiState.Loading -> {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                }
+                is FruitApiState.Empty -> {
+                    Text(text = "Empty")
+                }
             }
         }
     }
-
-
-
-
 }
+
 
 @Preview
 @Composable
--- app/src/main/java/com/eslirodrigues/fruitmacro/viewmodel/FruitViewModel.kt
+++ app/src/main/java/com/eslirodrigues/fruitmacro/viewmodel/FruitViewModel.kt
@@ -2,14 +2,13 @@ package com.eslirodrigues.fruitmacro.viewmodel
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eslirodrigues.fruitmacro.data.model.Fruit
 import com.eslirodrigues.fruitmacro.data.repository.FruitRepository
 import com.eslirodrigues.fruitmacro.util.FruitApiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onStart
@@ -22,8 +21,36 @@ class FruitViewModel @Inject constructor(
 ): ViewModel() {
     val response: MutableState<FruitApiState> = mutableStateOf(FruitApiState.Empty)
 
+    private var fruitList = listOf<Fruit>()
+    private var cachedFruitList = listOf<Fruit>()
+    private var isSearchStarting = true
+
     init {
         getAllFruits()
+    }
+
+    fun searchFruitList(query: String) {
+        val listToSearch = if(isSearchStarting) {
+            fruitList
+        } else {
+            cachedFruitList
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            if(query.isEmpty()) {
+                fruitList = cachedFruitList
+                response.value = FruitApiState.Success(fruitList)
+                return@launch
+            }
+            val results = listToSearch.filter {
+                it.name.contains(query.trim(), ignoreCase = true)
+            }
+            if(isSearchStarting) {
+                cachedFruitList = fruitList
+                isSearchStarting = false
+            }
+            fruitList = results
+            response.value = FruitApiState.Success(fruitList)
+        }
     }
 
     fun getAllFruits() = viewModelScope.launch {
@@ -35,6 +62,7 @@ class FruitViewModel @Inject constructor(
                 response.value = FruitApiState.Failure(it)
             }.collect {
                 response.value = FruitApiState.Success(it)
+                fruitList = it
             }
     }
 }
